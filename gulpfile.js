c41efea0b40c205a108acc7e058fe3d4e5914c13
James Cox-Morton <th3james@fastmail.fm>
2014-10-31 14:33:36 +0000

    Make gulpfile compile all files and concatenated files

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,55 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var coffee = require('gulp-coffee');
+var order = require('gulp-order');
+var concat = require('gulp-concat');
+var _ = require('lodash');
 
-var paths = {src: 'src/**/*.coffee'}
+var fileNames = {
+  src: [
+    'remote-helpers',
+    'remote-response-validator',
+    'updateable-views',
+    'remote-action',
+    'remote-confirm',
+    'remote-modal-view'
+  ]
+}
 
-gulp.task('coffee', function () {
-  gulp.src(paths.src)
+coffeescriptFiles = function() {
+  return _.map(fileNames.src, function(fileName) {
+    return "src/" + fileName + ".coffee";
+  })
+}
+
+javascriptFiles = function() {
+  return _.map(fileNames.src, function(fileName) {
+    return "dist/" + fileName + ".js";
+  })
+}
+
+console.log(coffeescriptFiles());
+
+compileCoffee = function() {
+  return gulp.src(coffeescriptFiles())
     .pipe(coffee({bare: true}).on('error', gutil.log))
+}
+
+gulp.task('compileSrc', function () {
+  compileCoffee().pipe(gulp.dest('./dist/'))
+});
+
+gulp.task('concatenateSrc', function() {
+  compileCoffee()
+    .pipe(order(javascriptFiles()))
+    .pipe(concat('bellhopper.js'))
     .pipe(gulp.dest('./dist/'))
 });
 
 // Rerun the task when a file changes
 gulp.task('watch', function() {
-  gulp.watch(paths.src, ['coffee']);
+  gulp.watch(coffeescriptFiles(), ['compileSrc', 'concatenateSrc']);
 });
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['coffee', 'watch']); 
+gulp.task('default', ['compileSrc', 'concatenateSrc', 'watch']); 
